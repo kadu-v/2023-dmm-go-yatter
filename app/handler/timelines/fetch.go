b0f57077b635675dbcebcda6ea68dbee388e65b0
kadu-v <kazuonsunday@yahoo.co.jp>
2023-08-04 10:57:07 +0900

    public timelineのクエリのパースの上限値をチェック

--- app/handler/timelines/fetch.go
+++ app/handler/timelines/fetch.go
@@ -16,7 +16,7 @@ type QueryParams struct {
 
 func (h *handler) FetchPublicTimeline(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	queryParams, err := queryParamsParser(r)
+	queryParams, err := queryParamsParser(r, 40 /* defulat limit */, 80 /* max limit */)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
@@ -35,7 +35,7 @@ func (h *handler) FetchPublicTimeline(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func queryParamsParser(r *http.Request) (*QueryParams, error) {
+func queryParamsParser(r *http.Request, dlftLimit int64, maxLimit int64) (*QueryParams, error) {
 	booleanParamParserWithDefualt := func(paramKey string, dflt bool) (bool, error) {
 		paramValue := r.FormValue(paramKey)
 		if paramValue == "" {
@@ -44,7 +44,7 @@ func queryParamsParser(r *http.Request) (*QueryParams, error) {
 
 		v, err := strconv.ParseBool(paramValue)
 		if err != nil {
-			return false, fmt.Errorf("Invalid query parameter for %s: %s", paramKey, err)
+			return false, fmt.Errorf("invalid query parameter for %s: %s", paramKey, err)
 		}
 
 		return v, nil
@@ -58,7 +58,7 @@ func queryParamsParser(r *http.Request) (*QueryParams, error) {
 
 		v, err := strconv.ParseInt(paramValue, 10 /* base */, 64 /* bitSize */)
 		if err != nil {
-			return -1, fmt.Errorf("Invalid query parameter for %s: %s", paramKey, err)
+			return -1, fmt.Errorf("invalid query parameter for %s: %s", paramKey, err)
 		}
 
 		return v, nil
@@ -69,9 +69,11 @@ func queryParamsParser(r *http.Request) (*QueryParams, error) {
 		return nil, err
 	}
 
-	limit, err := integerParamParserWithDefualt("limit", 40)
+	limit, err := integerParamParserWithDefualt("limit", dlftLimit)
 	if err != nil {
 		return nil, err
+	} else if !(0 <= limit && limit <= maxLimit) {
+		return nil, fmt.Errorf("exceed the limitation of public time lines: [defulat limit] \"%d\", [max limit] \"%d\", [current limit]: \"%d\"", dlftLimit, maxLimit, limit)
 	}
 
 	sinceID, err := integerParamParserWithDefualt("since_id", 0)
